742db9918198388dae05912791177b25463a72d3
vijay-garg-tpc <vijay.garg@tpcus.com>
2018-11-22 20:08:13 -0500

    initial changes by Nikhil

---
+++ html files/aboutus.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Gary Peter Group</title>
+<link href="../css_files/aboutus.css" type="text/css" rel="stylesheet" />
+<link href="https://fonts.googleapis.com/css?family=Raleway|Roboto" rel="stylesheet">
+</head>
+<body>
+<div class="image">
+<div>
+<h2 class="heading"><a href="aboutus.html">Gary Peter Group</a></h2>
+<div class="firstulist">
+<a href="#" class="active dropbtn">About</a>
+<a href="#" class="dropbtn">Investment Approach</a>
+<a href="#" class="dropbtn">Humanitarian Work</a>
+<a href="#" class="dropbtn">Contact Us</a>
+</div>
+</div>
+<br />
+<br />
+<br />
+<div class="about">
+<p class="aboutus"><b>About</b></p>
+</div>
+</div>
+<br />
+<br />
+<br />
+<br />
+<div class="first">
+<p>Gary Peter Group is a USA based private equity firm. We are pioneer in investment sector as we follow ethical and non-corrupt practices for greater good of overall startup community.</p>
+<p>Our investments are primarily focused towards the following industries:</p>
+<ul>
+<li>New Information technology</li>
+<li>Consulting Businesses</li>
+</ul>
+<p>Gary Peter Group firmly believes in investing in companies which has strong vision in addition to robust growth potential with long term sustainability. We invest in companies which are set to be market leaders of tomorrow.</p>
+<p>Our team of experienced industry professionals specializes in investing in new information technology startups and consulting businesses. The investment made by us aims to maximize the startups success. Gary Peter Group’s strategic partnership with management team ensures long term growth through a combination of strategic capital and operational insight.</p>
+<p>Gary Peter Group is the perfect choice for your investment needs because:</p>
+<ul>
+<li>We have experience of the industry spanning over 15 years.</li>
+<li>Our expertise in Information Technology and Consulting business is unparalleled.</li>
+<li>Gary Peter Group understands that starting from scratch is difficult. Our management team lends unconditional support and we assist you at every stage of the journey.</li>
+</ul>
+<h2>Our Goal</h2>
+<p>Gary Peter Group’s primary objective is to nourish the ever growing startup culture. We believe in lending financial and moral support to deserving entrepreneurs.</p>
+<p>Our goal is to ensure that the startup maintains its growth momentum and benefit the society through innovation.</p>
+</div>
+<br />
+<footer>
+<p><b>Copyrights ©2016-17 by Gary Peter Group</b></p>
+</footer>
+
+</body>
+</html>
