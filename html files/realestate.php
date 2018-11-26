67410632a4b9a12a52580e32ef4739026924ca69
nikhilkhapre <45065741+nikhilkhapre@users.noreply.github.com>
2018-11-26 16:31:00 +0530

    reduced code and links updated

--- html files/realestate.php
+++ html files/realestate.php
@@ -3,84 +3,28 @@
 <html lang="en-US" prefix="og: http://ogp.me/ns#">
 <head>
 <meta charset="UTF-8" />
-<link rel="profile" href="http://gmpg.org/xfn/11" />
-<link rel="pingback" href="http://garypeter.com/xmlrpc.php" />
 <link href="../css_files/common1.css" type="text/css" rel="stylesheet" />
 <link href="https://fonts.googleapis.com/css?family=Raleway|Roboto" rel="stylesheet">
-<title>Real Estate Fund - Gary Peter Group</title>
-
-<!-- This site is optimized with the Yoast SEO plugin v5.9.3 - https://yoast.com/wordpress/plugins/seo/ -->
-<link rel="canonical" href="http://garypeter.com/real-estate-fund/" />
-<meta property="og:locale" content="en_US" />
-<meta property="og:type" content="article" />
-<meta property="og:title" content="Real Estate Fund - Gary Peter Group" />
-<meta property="og:description" content="The Real Estate industry is as old as human civilization. The recorded earliest forms of real estate properties were man-made caves and tree houses. In present 21st century the real estate sector is blooming with glitzy apartments, sky-high office buildings, villas, row houses etc. For ages the real estate sector attracted investors as it was safe and financial meltdowns had little impact on it. Today&hellip;" />
-<meta property="og:url" content="http://garypeter.com/real-estate-fund/" />
-<meta property="og:site_name" content="Gary Peter Group" />
-<meta property="article:section" content="Investment Approach" />
-<meta property="article:published_time" content="2016-04-20T16:21:58-05:00" />
-<meta property="article:modified_time" content="2016-05-20T16:22:57-05:00" />
-<meta property="og:updated_time" content="2016-05-20T16:22:57-05:00" />
-<meta name="twitter:card" content="summary" />
-<meta name="twitter:description" content="The Real Estate industry is as old as human civilization. The recorded earliest forms of real estate properties were man-made caves and tree houses. In present 21st century the real estate sector is blooming with glitzy apartments, sky-high office buildings, villas, row houses etc. For ages the real estate sector attracted investors as it was safe and financial meltdowns had little impact on it. Today&hellip; " />
-<meta name="twitter:title" content="Real Estate Fund - Gary Peter Group" />
-<script type='application/ld+json'>{"@context":"http:\/\/schema.org","@type":"WebSite","@id":"#website","url":"http:\/\/garypeter.com\/","name":"Gary Peter Group","potentialAction":{"@type":"SearchAction","target":"http:\/\/garypeter.com\/?s={search_term_string}","query-input":"required name=search_term_string"}}</script>
-<script type='application/ld+json'>{"@context":"http:\/\/schema.org","@type":"Organization","url":"http:\/\/garypeter.com\/real-estate-fund\/","sameAs":[],"@id":"#organization","name":"Gary Peter Group, LLC","logo":""}</script>
-<!-- / Yoast SEO plugin. -->
-
-<link rel='dns-prefetch' href='//platform-api.sharethis.com' />
-<link rel='dns-prefetch' href='//maxcdn.bootstrapcdn.com' />
-<link rel='dns-prefetch' href='//fonts.googleapis.com' />
-<link rel='dns-prefetch' href='//s.w.org' />
-<link rel="alternate" type="application/rss+xml" title="Gary Peter Group &raquo; Feed" href="http://garypeter.com/feed/" />
-<link rel="alternate" type="application/rss+xml" title="Gary Peter Group &raquo; Comments Feed" href="http://garypeter.com/comments/feed/" />
-<link rel="alternate" type="application/rss+xml" title="Gary Peter Group &raquo; Real Estate Fund Comments Feed" href="http://garypeter.com/real-estate-fund/feed/" />
-		<script type="text/javascript">
-			window._wpemojiSettings = {"baseUrl":"https:\/\/s.w.org\/images\/core\/emoji\/2.4\/72x72\/","ext":".png","svgUrl":"https:\/\/s.w.org\/images\/core\/emoji\/2.4\/svg\/","svgExt":".svg","source":{"concatemoji":"http:\/\/garypeter.com\/wp-includes\/js\/wp-emoji-release.min.js?ver=4.9.3"}};
-			!function(a,b,c){function d(a,b){var c=String.fromCharCode;l.clearRect(0,0,k.width,k.height),l.fillText(c.apply(this,a),0,0);var d=k.toDataURL();l.clearRect(0,0,k.width,k.height),l.fillText(c.apply(this,b),0,0);var e=k.toDataURL();return d===e}function e(a){var b;if(!l||!l.fillText)return!1;switch(l.textBaseline="top",l.font="600 32px Arial",a){case"flag":return!(b=d([55356,56826,55356,56819],[55356,56826,8203,55356,56819]))&&(b=d([55356,57332,56128,56423,56128,56418,56128,56421,56128,56430,56128,56423,56128,56447],[55356,57332,8203,56128,56423,8203,56128,56418,8203,56128,56421,8203,56128,56430,8203,56128,56423,8203,56128,56447]),!b);case"emoji":return b=d([55357,56692,8205,9792,65039],[55357,56692,8203,9792,65039]),!b}return!1}function f(a){var c=b.createElement("script");c.src=a,c.defer=c.type="text/javascript",b.getElementsByTagName("head")[0].appendChild(c)}var g,h,i,j,k=b.createElement("canvas"),l=k.getContext&&k.getContext("2d");for(j=Array("flag","emoji"),c.supports={everything:!0,everythingExceptFlag:!0},i=0;i<j.length;i++)c.supports[j[i]]=e(j[i]),c.supports.everything=c.supports.everything&&c.supports[j[i]],"flag"!==j[i]&&(c.supports.everythingExceptFlag=c.supports.everythingExceptFlag&&c.supports[j[i]]);c.supports.everythingExceptFlag=c.supports.everythingExceptFlag&&!c.supports.flag,c.DOMReady=!1,c.readyCallback=function(){c.DOMReady=!0},c.supports.everything||(h=function(){c.readyCallback()},b.addEventListener?(b.addEventListener("DOMContentLoaded",h,!1),a.addEventListener("load",h,!1)):(a.attachEvent("onload",h),b.attachEvent("onreadystatechange",function(){"complete"===b.readyState&&c.readyCallback()})),g=c.source||{},g.concatemoji?f(g.concatemoji):g.wpemoji&&g.twemoji&&(f(g.twemoji),f(g.wpemoji)))}(window,document,window._wpemojiSettings);
-		</script>
-<link rel='stylesheet' id='font-awesome-css'  href='https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css?ver=1.11' type='text/css' media='all' />
-<link rel='stylesheet' id='the-social-links-style-css'  href='http://garypeter.com/wp-content/plugins/the-social-links/assets/css/style.css?ver=4.9.3' type='text/css' media='all' />
+<title>Gary Peter Group</title>
 <link rel='stylesheet' id='freesiaempire-style-css'  href='http://garypeter.com/wp-content/themes/freesia-empire/style.css?ver=4.9.3' type='text/css' media='all' />
-<link rel='stylesheet' id='freesiaempire_google_fonts-css'  href='//fonts.googleapis.com/css?family=Roboto%3A400%2C300%2C500%2C700&#038;ver=4.9.3' type='text/css' media='all' />
-<link rel='stylesheet' id='genericons-css'  href='http://garypeter.com/wp-content/themes/freesia-empire/genericons/genericons.css?ver=3.4.1' type='text/css' media='all' />
-<link rel='stylesheet' id='freesiaempire-responsive-css'  href='http://garypeter.com/wp-content/themes/freesia-empire/css/responsive.css?ver=4.9.3' type='text/css' media='all' />
-<link rel='stylesheet' id='freesiaempire-animate-css'  href='http://garypeter.com/wp-content/themes/freesia-empire/assets/wow/css/animate.min.css?ver=4.9.3' type='text/css' media='all' />
 <link rel='stylesheet' id='wp-add-custom-css-css'  href='http://garypeter.com?display_custom_css=css&#038;ver=4.9.3' type='text/css' media='all' />
-<script type='text/javascript' src='http://garypeter.com/wp-includes/js/jquery/jquery.js?ver=1.12.4'></script>
-<script type='text/javascript' src='http://garypeter.com/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.4.1'></script>
-<script type='text/javascript' src='http://garypeter.com/wp-content/themes/freesia-empire/js/freesiaempire-main.js?ver=4.9.3'></script>
-<script type='text/javascript' src='http://garypeter.com/wp-content/themes/freesia-empire/js/freesiaempire-sticky-scroll.js?ver=4.9.3'></script>
-<!--[if lt IE 9]>
-<script type='text/javascript' src='http://garypeter.com/wp-content/themes/freesia-empire/js/html5.js?ver=3.7.3'></script>
-<![endif]-->
-<script type='text/javascript' src='http://garypeter.com/wp-content/themes/freesia-empire/assets/wow/js/wow.min.js?ver=4.9.3'></script>
-<script type='text/javascript' src='http://garypeter.com/wp-content/themes/freesia-empire/assets/wow/js/wow-settings.js?ver=4.9.3'></script>
-<script type='text/javascript' src='//platform-api.sharethis.com/js/sharethis.js#product=ga'></script>
-<link rel='https://api.w.org/' href='http://garypeter.com/wp-json/' />
-<link rel="EditURI" type="application/rsd+xml" title="RSD" href="http://garypeter.com/xmlrpc.php?rsd" />
-<link rel="wlwmanifest" type="application/wlwmanifest+xml" href="http://garypeter.com/wp-includes/wlwmanifest.xml" /> 
-<meta name="generator" content="WordPress 4.9.3" />
-<link rel='shortlink' href='http://garypeter.com/?p=382' />
-<link rel="alternate" type="application/json+oembed" href="http://garypeter.com/wp-json/oembed/1.0/embed?url=http%3A%2F%2Fgarypeter.com%2Freal-estate-fund%2F" />
-<link rel="alternate" type="text/xml+oembed" href="http://garypeter.com/wp-json/oembed/1.0/embed?url=http%3A%2F%2Fgarypeter.com%2Freal-estate-fund%2F&#038;format=xml" />
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 	</head>
-<body class="post-template-default single single-post postid-382 single-format-standard custom-background no-sidebar-layout small_image_blog  boxed-layout">
+<body class="home page-template-default page page-id-140 custom-background no-sidebar-layout small_image_blog  boxed-layout">
 <div class="image">
-<div>
-<h2 class="heading"><a href="aboutus.html">Gary Peter Group</a></h2>
+<div class="menuitems">
+<h2 class="heading"><a href="aboutus.php">Gary Peter Group</a></h2>
 <div class="firstulist">
-<a href="#" class="dropbtn">About</a>
-<a href="#" class=" active dropbtn">Investment Approach</a>
-<a href="#" class="dropbtn">Humanitarian Work</a>
-<a href="#" class="dropbtn">Contact Us</a>
+<a href="aboutus.php" class="dropbtn">About</a>
+<a href="investmentapproach.php" class="active dropbtn">Investment Approach</a>
+<a href="humanitarianwork.php" class="dropbtn">Humanitarian Work</a>
+<a href="contactus.php" class="dropbtn">Contact Us</a>
 </div>
 </div>
 <br />
 <br />
-<div class="invest2">
-<p class="realestate"><b>Real Estate Fund</b></p>
+<div class="strategy">
+<p class="invstrategy"><b>Real Estate Fund</b></p>
 </div>
 </div>
 <br />
@@ -93,14 +37,6 @@
 <div id="content">
 <div class="container clearfix">
 	<main id="main" class="site-main clearfix">
-							<article class="post-format format- post-382 post type-post status-publish format-standard hentry category-investment clearfix" id="post-382">
-								<header class="entry-header">
-									<div class="entry-meta">
-												<span class="author vcard">Author :<a href="http://garypeter.com/author/gpcadmin/" title="gpcadmin">
-						gpcadmin </a></span> <span class="posted-on">Date  :<a title="4:21 pm" href="http://garypeter.com/real-estate-fund/">
-						April 20, 2016 </a></span>
-											</div> <!-- .entry-meta -->
-								</header> <!-- .entry-header -->
 				<div class="entry-content">
 				<p>The Real Estate industry is as old as human civilization. The recorded earliest forms of real estate properties were man-made caves and tree houses. In present 21st century the real estate sector is blooming with glitzy apartments, sky-high office buildings, villas, row houses etc.<br />
 For ages the real estate sector attracted investors as it was safe and financial meltdowns had little impact on it. Today the real estate industry is faced with several troubles such as:<br />
@@ -120,13 +56,13 @@ Our dedicated capital and expertise, combined with your enthusiasm will create a
 Submit your application today and we will get in touch with you!</p>
 				</div> <!-- .end entry-content -->
 									<footer class="entry-footer"> <span class="cat-links">
-						Category : <a href="http://garypeter.com/category/investment/" rel="category tag">Investment Approach</a> </span> <!-- .cat-links -->
+						Category : <a href="investmentapproach.php" rel="category tag">Investment Approach</a> </span> <!-- .cat-links -->
 											</footer> <!-- .entry-meta -->
 								</article>
 		</main> <!-- #main -->
 			<ul class="default-wp-page clearfix">
-			<li class="previous"> <a href="ourphilosophy.html" rel="prev"><span class="meta-nav">&larr;</span> Our Philosophy</a> </li>
-			<li class="next"> <a href="startupfund.html" rel="next">Startup Fund <span class="meta-nav">&rarr;</span></a> </li>
+			<li class="previous"> <a href="startupfund.php" rel="prev"><span class="meta-nav">&larr;</span> Startup Fund</a> </li>
+			<li class="next"> <a href="ourphilosophy.php" rel="next">Our Philosophy <span class="meta-nav">&rarr;</span></a> </li>
 		</ul>
 			
 <div id="comments" class="comments-area">
